857ca30e7f8d1e358dc76010d3aa874b63656eda
haris-p <harisujiantoro09@gmail.com>
2021-09-12 14:10:59 +0700

    gini ajaterus

--- latihan/latihan3.py
+++ latihan/latihan3.py
@@ -63,7 +63,7 @@ def hasilhitung():
         print("Pajak :", pajak)
         totalkeseluruhan = (a - diskon) + pajak
         print("Total Keseluruhan: ",totalkeseluruhan)
-        print("=" * 20)
+        print("=" * 21)
         pembayaran = int(input("Masukan Pembayaran: "))
         bayar = (pembayaran - totalkeseluruhan)
         print("Kembalian :", bayar)
